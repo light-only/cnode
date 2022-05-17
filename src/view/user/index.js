68adc1380106306e0a17382850c4c1ea8125cf01
lvxiaoliang <1480418586@qq.com>
2022-05-17 09:21:50 +0800

    关于我们和新手入门代码提交

--- src/view/user/index.js
+++ src/view/user/index.js
@@ -1,10 +1,11 @@
-import {Card} from 'antd'
+import {Card,Avatar} from 'antd'
 import {useUserList} from "../../store/action";
 import {useEffect} from "react";
 import {useParams} from "react-router-dom";
 import {useSelector} from "react-redux";
 import TopicsList from "../../component/TopicsList";
-
+import FromNow from "../../component/FromNow";
+import {UserOutlined} from "@ant-design/icons";
 export default ()=>{
     let {loginname} = useParams();
     console.log(loginname)
@@ -13,13 +14,21 @@ export default ()=>{
         getData(loginname)
     },[loginname]);
     const {loading,data} = useSelector(state=>state.user);
-    const {recent_replies,recent_topics,score,avatar_url,create_at} = data;
+    const {recent_replies,recent_topics,score,avatar_url,githubUsername, create_at} = data;
     return <div className='user_index'>
        <Card
            loading={loading}
            data={111}
-           title={'最近创建的话题'}
+           className='user_details'
        >
+           <Avatar
+             size={80}
+             icon={<UserOutlined />}
+             src={avatar_url}
+
+           />
+           <p style={{marginTop:20}}>用户名：{loginname} 注册时间：{<FromNow data={create_at}/>} 积分：{score}</p>
+           <p>github：<a href={`https://github.com/${githubUsername}`} target={'_blank'}>https://github.com/{githubUsername}</a></p>
        </Card>
         <Card
             loading={loading}
